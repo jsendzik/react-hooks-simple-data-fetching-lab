78884f58570e2766d8f03f07d9b9bef7de9d4b59
Jacob Sendzik <jake.sendzik@gmail.com>
2023-11-13 12:03:15 -0500

    Complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,34 @@
 // create your App component here
+import React, { useEffect, useState } from "react";
+
+function App() {
+    const [dogUrl, setDogUrl] = useState("");
+    const [image, setImage] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+  
+    useEffect(() => {
+      fetch("https://dog.ceo/api/breeds/image/random")
+        .then((r) => r.json())
+        .then((data) => {
+          setDogUrl(data.message);
+          setIsLoading(false);
+        })
+        .catch((error) => {
+          console.log("Error fetching dog image:", error);
+        });
+    }, []);
+  
+    useEffect(() => {
+      if (dogUrl) {
+        setImage(<img alt="A Random Dog" src={dogUrl} />);
+      }
+    }, [dogUrl]);
+  
+    return (
+      <div>
+        <p>{isLoading ? "Loading..." : image}</p>
+      </div>
+    );
+  }
+
+export default App
